5090b240a292984868f8d0743aeb8ecf49f28e9e
Puzzle <puzzle1536@gmail.com>
2014-04-11 18:11:28 +0200

    Optimize space in compose window layout

--- ui.js
+++ ui.js
@@ -396,8 +396,8 @@ function rcube_mail_ui()
     bottom.css('height', Math.max(minheight, bh) + 'px');
     form.css('overflow', ovflw > 0 ? 'auto' : 'hidden');
 
-    w = body.parent().width() - 5;
-    h = body.parent().height() - 16;
+    w = body.parent().width() - 10;
+    h = body.parent().height() - 50;
     body.width(w).height(h);
 
     $('#composebody_tbl').width((w+8)+'px').height('').css('margin-top', '1px');
